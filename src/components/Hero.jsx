999e0dc2a4246b42adad52958932452547c0a26f
Iraicy <iraicyahh@gmail.com>
2021-09-03 02:16:46 -0500

    pagina registro

---
+++ src/components/Hero.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Formulario from "./Formulario";
+import "./hero.css";
+
+const Hero = () => {
+  return (
+    <div className="wrapper">
+      <h1 className="title">Incap</h1>
+
+      <Formulario />
+    </div>
+  );
+};
+
+export default Hero;
